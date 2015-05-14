c2957958ab0224bd57e648d984fef8ee3872ff04
Sushant Kumar <sushant@cronj.com>
2015-05-14 17:31:11 +0530

    login

--- server/api/product/index.js
+++ server/api/product/index.js
@@ -8,6 +8,7 @@ router.get('/' , controller.index);
 router.get('/featuredProducts' , controller.selectRandCatProdt);
 router.get('/product/:id', controller.productDetail);
 router.get('category/:category',controller.productByCategory);
+router.get('/featured', controller.selectRandCatProdt);
 router.post('/create', controller.create);
 router.put('/product/:id', controller.updateProduct);
 router.delete('/product/:id', controller.deleteProduct);
